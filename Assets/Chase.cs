715ca4f28bce2ad8f4b48d03b3a9c7b0a5062d46
axgtz <axguti@gmail.com>
2016-11-29 19:06:16 -0600

    added Assets meta files outside of folders

---
+++ Assets/Chase.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Chase : MonoBehaviour
+{
+    public Transform Camera;
+    public float MoveSpeed = 0.1f;
+    public float MaxDist = 10;
+    public float MinDist = 5;
+    public Text menuText;
+    void Start()
+    {
+        //wayPoint = GameObject.Find("wayPoint");
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+
+    {
+        /*
+        wayPointPos = new Vector3(wayPoint.transform.position.x, transform.position.y, wayPoint.transform.position.z);
+        //Here, the zombie's will follow the waypoint.
+        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+        */
+
+
+        transform.LookAt(Camera);
+        //transform.position = new Vector3(RigidBodyFPSController.position.x, transform.position.y - 0.5f, transform.position.z);
+        if (Vector3.Distance(transform.position, Camera.position) <= MinDist)
+        {
+            menuText.text = "You're dead.";
+
+        }
+        if (Vector3.Distance(transform.position, Camera.position) >= MinDist){
+
+           transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+
+
+            if (Vector3.Distance(transform.position,Camera.position) <= MaxDist){
+                //Here Call any function U want Like Shoot at here or something
+            }
+
+        }
+    
+    }
+    
+}
---
+++ Assets/WeepingAI.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeepingAI : MonoBehaviour
+{
+
+    public Transform RigidBodyFPSController;
+    MeshRenderer objectRenderer;
+    Vector3 originalPos;
+    public bool inTrigger = false;
+    public float dotProduct;
+    bool canHurt = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        originalPos = transform.position;
+        objectRenderer = GetComponentInChildren<MeshRenderer>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!inTrigger)
+        {
+            transform.position = originalPos;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+        if (inTrigger)
+        {
+            if (CheckIfOffScreen())
+            {
+                //All move code here;
+                transform.LookAt(new Vector3(RigidBodyFPSController.position.x, transform.position.y, RigidBodyFPSController.position.z));
+                if (Vector3.Distance(RigidBodyFPSController.position, transform.position) > 5)
+                {
+                    transform.position = RigidBodyFPSController.position - RigidBodyFPSController.forward;
+                }
+            }
+        }
+    }
+
+
+    //If hes in his quadrant
+    IEnumerator OnTriggerStay(Collider col)
+    {
+        yield return new WaitForFixedUpdate();
+        if (col.CompareTag("WeepingQuadrant"))
+        {
+            //Check if the player can see it
+            inTrigger = true;
+
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("WeepingQuadrant"))
+            inTrigger = false;
+
+    }
+
+    bool CheckIfOffScreen()
+    {
+        Vector3 playerFwd = RigidBodyFPSController.forward;
+        Vector3 targetDir = (RigidBodyFPSController.position - transform.position).normalized;
+        dotProduct = Vector3.Dot(playerFwd, targetDir);
+        if (dotProduct < -0.5)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+}
